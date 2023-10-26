640ceef19794f012c9afd8bb5c0b04ce8312c2a3
galentaris <galentaris01@gmail.com>
2023-10-26 22:36:13 +0700

    fix bug

--- diskusiBook/urls.py
+++ diskusiBook/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from main.views import show_main
+from diskusiBook.views import show_diskusi
 
 app_name = 'diskusiBook'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('diskusi-book/<int:id>/', show_diskusi, name='show_diskusi'),
 ]
