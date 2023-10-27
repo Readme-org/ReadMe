94ae7ed2d1191538a1896021bd9c9aa9bb1b0156
GhanyR <neoperkasa@gmail.com>
2023-10-27 17:13:11 +0700

    main

--- wishlistBook/static/js/main_script.js
+++ wishlistBook/static/js/main_script.js
@@ -41,42 +41,47 @@ function searchBooks(query) {
   }
   
   function displayBooks(books) {
-      const bookList = document.querySelector('.book-list');
-      bookList.innerHTML = ''; // Clear any previous results
-      books.forEach(book => {
-          const bookItem = document.createElement('div');
-          bookItem.className = 'book-item';
-  
-          const bookCover = document.createElement('img');
-          bookCover.src = book.volumeInfo.imageLinks.thumbnail;
-          bookCover.className = 'book-cover';
-  
-          const bookDetails = document.createElement('div');
-          bookDetails.className = 'book-details';
-  
-          const bookTitle = document.createElement('h2');
-          bookTitle.className = 'book-title';
-          bookTitle.innerText = book.volumeInfo.title;
-  
-          const bookDescription = document.createElement('p');
-          bookDescription.className = 'book-description';
-          bookDescription.innerText = book.volumeInfo.description;
-  
-          const bookRating = document.createElement('p');
-          bookRating.className = 'book-rating';
-          bookRating.innerText = `Rating: ${book.volumeInfo.averageRating}/5`;
-  
-          bookDetails.appendChild(bookTitle);
-          bookDetails.appendChild(bookDescription);
-          bookDetails.appendChild(bookRating);
-  
-          bookItem.appendChild(bookCover);
-          bookItem.appendChild(bookDetails);
-  
-          bookList.appendChild(bookItem);
-      });
-      document.getElementById('bookModal').style.display = 'flex';
-  }
+    const bookList = document.querySelector('.book-list');
+    bookList.innerHTML = ''; // Clear any previous results
+    books.forEach(book => {
+        const bookItem = document.createElement('div');
+        bookItem.className = 'book-item';
+
+        const bookCover = document.createElement('img');
+        bookCover.src = book.volumeInfo.imageLinks.thumbnail;
+        bookCover.className = 'book-cover';
+
+        const bookDetails = document.createElement('div');
+        bookDetails.className = 'book-details';
+
+        const bookTitle = document.createElement('h2');
+        bookTitle.className = 'book-title';
+        bookTitle.innerText = book.volumeInfo.title;
+
+        const bookDescription = document.createElement('p');
+        bookDescription.className = 'book-description';
+        bookDescription.innerText = book.volumeInfo.description;
+
+        const bookRating = document.createElement('p');
+        bookRating.className = 'book-rating';
+        bookRating.innerText = `Rating: ${book.volumeInfo.averageRating}/5`;
+
+        const bookAuthors = document.createElement('p');
+        bookAuthors.className = 'book-authors';
+        bookAuthors.innerText = `Authors: ${book.volumeInfo.authors.join(', ')}`;
+
+        bookDetails.appendChild(bookTitle);
+        bookDetails.appendChild(bookDescription);
+        bookDetails.appendChild(bookRating);
+        bookDetails.appendChild(bookAuthors);
+
+        bookItem.appendChild(bookCover);
+        bookItem.appendChild(bookDetails);
+
+        bookList.appendChild(bookItem);
+    });
+    document.getElementById('bookModal').style.display = 'flex';
+}
   
   function closeModal() {
       document.getElementById('bookModal').style.display = 'none';
@@ -85,20 +90,18 @@ function searchBooks(query) {
   // This is just a dummy function to simulate the behavior. 
   // Replace this function with your actual API call.
   function searchBooks(query) {
-      // For the purpose of this demonstration, 
-      // I'm using the sample book you provided.
-      const sampleBooks = [
-          {
-              volumeInfo: {
-                  title: "The Art Of Seduction",
-                  description: "Description here...",
-                  averageRating: 5,
-                  imageLinks: {
-                      thumbnail: "http://example.com/image.jpg"
-                  }
-              }
-          },
-          // ... Add more sample books as needed
-      ];
-      displayBooks(sampleBooks);
-  }
+    fetch('http://127.0.0.1:5000/AI_Search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({context: query})
+    })
+    .then(response => response.json())
+    .then(data => {
+        displayBooks(data.books);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
