c754c19c0258ba25291eb617304b6b9304d0387b
galentaris <galentaris01@gmail.com>
2023-10-27 14:36:49 +0700

    update authenticate

--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-from django.db import models
 
 class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     display_title = models.CharField(max_length=255)
-    authors = models.TextField()  # Daftar nama penulis, dipisahkan dengan koma
+    authors = models.TextField()
     image = models.TextField()
--- main/static/js/main_script.js
+++ main/static/js/main_script.js
@@ -102,3 +102,10 @@ function searchBooks(query) {
       ];
       displayBooks(sampleBooks);
   }
+
+document.querySelector(".show-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active")
+});
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active")
+});
