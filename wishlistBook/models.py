d72547884461c2bb1ff19189f96c1731ee8ddc0e
GoedangG <rizqi.bayu@ui.ac.id>
2023-10-28 09:27:28 +0700

    proggress

--- wishlistBook/models.py
+++ wishlistBook/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from main.models import Book
 
 # Create your models here.
+
+class WishlistBook(models.Model):
+    title = models.CharField(max_length=255)
+    display_title = models.CharField(max_length=255)
+    authors = models.TextField()
+    image = models.TextField()
