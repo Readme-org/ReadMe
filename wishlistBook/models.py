560d7d7b14e70019cf0d8eb40fd5968588354432
GoedangG <rizqi.bayu@ui.ac.id>
2023-10-29 01:08:43 +0700

    update views.py

--- wishlistBook/models.py
+++ wishlistBook/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from main.models import Book
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class WishlistBook(models.Model):
-    title = models.CharField(max_length=255)
-    display_title = models.CharField(max_length=255)
-    authors = models.TextField()
-    image = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
