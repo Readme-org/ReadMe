fa70114cad8b195d9fdeda23046a69f570fe1325
Jessica Ruth Damai Yanti Manurung <124943613+jessiruth@users.noreply.github.com>
2023-10-29 06:04:28 +0000

    Modify: Model rating

--- ratingBook/models.py
+++ ratingBook/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from main.models import Book
 
 # Create your models here.
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    rating = models.IntegerField()
-    description = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
