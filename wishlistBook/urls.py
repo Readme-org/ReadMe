5093cd5cf16238a0626c4b70f84cdc4ff0d40508
Bayu <rizqi.bayu@ui.ac.id>
2023-10-29 19:42:49 +0700

    ubah views dan wishlist.html

--- wishlistBook/urls.py
+++ wishlistBook/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from main.views import show_main
-from wishlistBook.views import show_wishlist
+from wishlistBook.views import show_wishlist, get_books_json, deleteWishlist
 
 app_name = 'wishlistBook'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('wishlist-book', show_wishlist, name='show_wishlist'),
+    path('get-books/', get_books_json, name='get_books_json'),
+    path('delete-wishlist/', deleteWishlist, name='deleteWishlist'),
 ]
--- wishlistBook/views.py
+++ wishlistBook/views.py
@@ -1,21 +1,35 @@
+import json
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from main.models import Book
+from django.urls import reverse
+from wishlistBook.models import WishlistBook
 from django.core import serializers
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required(login_url='/login')
 def show_wishlist(request):
-    books = Book.objects.filter(user=request.user)
+    books = WishlistBook.objects.filter(user=request.user)
     context = {
-        'books': books 
+        'name': request.user.username,
+        'books': books, 
     }
     return render(request, "wishlist.html", context)
 
 def get_books_json(request):
-    books_wishlist = Book.objects.filter(user=request.user)
+    books_wishlist = WishlistBook.objects.filter(user=request.user)
     wishlist = []
     for book in books_wishlist:
         wishlist.append({
             'title': book.book.title,
-            
+            'display_title': book.book.display_title,
+            'authors': book.book.authors,
+            'image': book.book.image,
         })
+    final = json.dumps(wishlist)
+    return HttpResponse(final, content_type ='application/json')
+
+def deleteWishlist(request, id):
+    wishlist = WishlistBook.objects.get(pk=id)
+    wishlist.delete()
+    return HttpResponse(b"Deleted", 201)
