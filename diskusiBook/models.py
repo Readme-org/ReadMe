674d1ad4c7e47982a49445a5d2990bd9a5e30ad7
WinotoHasyim <hasyimwinoto@gmail.com>
2023-10-29 12:01:20 +0700

    Draft module

--- diskusiBook/models.py
+++ diskusiBook/models.py
@@ -6,31 +6,22 @@ from django.contrib.auth.models import User
 from django.shortcuts import reverse
 
 # Create your models here.
-
-def get_default_post():
-    return Post.objects.get_or_create(content="test")[0]
-
-def get_default_book():
-    return Book.objects.get_or_create(title="test")[0]
-
-def get_default_comment():
-    return Comment.objects.get_or_create(content="test")[0]
     
 class Post(models.Model):
-    Book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    title = models.CharField(max_length=300)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     
 class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
