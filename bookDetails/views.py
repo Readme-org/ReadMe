eabaa6c7bda51501b93fb0f6a0bbef19fa801713
Jessica Ruth Damai Yanti Manurung <124943613+jessiruth@users.noreply.github.com>
2023-10-29 06:34:26 +0000

    Add Rating to bookDetails and fix bug

--- bookDetails/views.py
+++ bookDetails/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from main.models import Book
 from listBook.models import myBook
+from ratingBook.models import Rating
 
 # Create your views here.
 def show_details(request, id):
     book = Book.objects.get(pk = id)
+    reviews = Rating.objects.filter(book_id=id)
 
     context = {
         'name': request.user.username,
         'book': book,
+        'reviews': reviews,
     }
 
     return render(request, "details.html", context)
