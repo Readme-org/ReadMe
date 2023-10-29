5fdcfb42b1cce2cd58f9cd443928c19ce5cd1d0b
Jessica Ruth Damai Yanti Manurung <124943613+jessiruth@users.noreply.github.com>
2023-10-29 06:07:24 +0000

    Modify: Views and URLs

--- ratingBook/urls.py
+++ ratingBook/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from ratingBook.views import show_rating, debug
+from ratingBook import views
 
 app_name = 'ratingBook'
 
 urlpatterns = [
-    path('<int:id>/', show_rating, name='show_rating'),
-    path('debug/', debug, name='debug')
+    path('', views.show_rating, name='show_rating'),
+    path('reviews/<int:id>/', views.get_rating_json, name='get_rating'),
+    path('reviews/<int:id>/update/', views.show_rating_to_update, name='show_rating_to_update'),
+    path('reviews/<int:id>/delete/', views.delete_rating, name='delete_rating'),
+    path('reviews/create/', views.create_rating_ajax, name='create_rating'),
+    path('reviews/update/', views.update_rating_ajax, name='update_rating'),
 ]
--- ratingBook/views.py
+++ ratingBook/views.py
@@ -2,20 +2,28 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseForbidden
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
-from .models import Rating
+from .models import Rating, Book
 
 # Create your views here.
-def debug(request):
-    return render(request, 'rating.html')
+def show_rating(request):
+    books = Book.objects.all()
 
-def show_rating(request, id):
-    ratings = Rating.objects.filter(book_id=id)
-    
     context = {
-        'ratings': ratings
+        'books': books
     }
-    
+
     return render(request, 'rating.html', context)
+
+def show_rating_to_update(request, id):
+    book = Book.objects.get(id=id)
+    rating = Rating.objects.get(book_id=id)
+
+    context = {
+        'book': book,
+        'rating': rating
+    }
+
+    return render(request, 'update_rating.html', context)
 
 def get_rating_json(request, id):
     ratings = Rating.objects.filter(book_id=id)
@@ -28,16 +36,12 @@ def get_rating_json(request, id):
 def create_rating_ajax(request):
     if request.method == 'POST':
         user = request.user
-        book_id = request.POST['book_id']
+        book_id = request.POST['book']
+        book = Book.objects.get(id=book_id)
         rating = request.POST['rating']
-        description = request.POST['description']
-        
-        Rating.objects.create(
-            user=user,
-            book_id=book_id,
-            rating=rating,
-            description=description
-        )
+        message = request.POST['message']
+
+        rating = Rating.objects.create(user=user, book=book, rating=rating, message=message)
         
         return HttpResponse('Rating created successfully', status=201)
     return HttpResponseForbidden('Allowed only via POST')
@@ -46,26 +50,19 @@ def create_rating_ajax(request):
 def update_rating_ajax(request):
     if request.method == 'POST':
         user = request.user
-        book_id = request.POST['book_id']
+        book_id = request.POST['book']
         rating = request.POST['rating']
-        description = request.POST['description']
-        
-        rating = Rating.objects.get(user=user, book_id=book_id)
-        rating.rating = rating
-        rating.description = description
-        rating.save()
+        message = request.POST['message']
+
+        rating = Rating.objects.filter(user=user, book_id=book_id).update(rating=rating, message=message)
         
         return HttpResponse('Rating updated successfully', status=201)
     return HttpResponseForbidden('Allowed only via POST')
 
 @login_required(login_url='main:login')
-def delete_rating_ajax(request):
-    if request.method == 'POST':
-        user = request.user
-        book_id = request.POST['book_id']
-        
-        rating = Rating.objects.get(user=user, book_id=book_id)
+def delete_rating(request, id):
+    rating = Rating.objects.get(id=id)
+    if request.user == rating.user:
         rating.delete()
-        
-        return HttpResponse('Rating deleted successfully', status=201)
-    return HttpResponseForbidden('Allowed only via POST')
+        return redirect('ratingBook:show_rating')
+    return HttpResponseForbidden('You are not allowed to delete this rating')
