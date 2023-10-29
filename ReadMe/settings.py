6204bc31f608333b4c875ce7fb4655a8b3c4e38d
Jessica Ruth Damai Yanti Manurung <124943613+jessiruth@users.noreply.github.com>
2023-10-29 06:06:56 +0000

    Modify: Set back settings.py

--- ReadMe/settings.py
+++ ReadMe/settings.py
@@ -61,9 +61,7 @@ ROOT_URLCONF = 'ReadMe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            'main/templates',
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
